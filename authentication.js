f735dd8147323f5fbcd32d91fe2afc6c6a7ae08d
Sujal Soni <112640952+Sujal-2820@users.noreply.github.com>
2023-03-30 23:56:56 +0530

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -4,8 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 
 passport.use(new GoogleStrategy({
-    clientID: '323558499472-3btd73gnq8a4tb0d2t94q2mnedlb9dme.apps.googleusercontent.com',
-    clientSecret: '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',
+    clientID: 'Client_ID',
+    clientSecret: 'ClientSecret',
     callbackURL: "http://localhost:3000/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
